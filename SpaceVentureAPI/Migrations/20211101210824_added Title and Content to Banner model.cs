410ef6282aefd997191c5536d7628fbd2a8278fa
Daniel Dollerup <d.dollerup@gmail.com>
2021-11-01 22:17:46 +0100

    Added Title and Content to Banner model

---
+++ SpaceVentureAPI/Migrations/20211101210824_added Title and Content to Banner model.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpaceAdventureAPI.Migrations
+{
+    public partial class addedTitleandContenttoBannermodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Banners",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Banners",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Banners");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Banners");
+        }
+    }
+}
